0b2dbb2b20896010687130345715ecef81c4acbf
DatDoc <uncledrew2youngblood@gmail.com>
2020-10-09 13:16:21 +0700

    update

--- sGA/ga-python.py
+++ sGA/ga-python.py
@@ -1,11 +1,14 @@
 import numpy as np
 import datetime
+import random
 
 
 # generate new gen
-def create_gen(panjang_target):
-    random_number = np.random.randint(32, 126, size=panjang_target)
-    gen = ''.join([chr(i) for i in random_number])
+def create_gen(population_size):
+    gen = ""
+    for i in range(population_size):
+        temp = str(random.randint(0, 1))
+        gen += temp
     return gen
 
 
@@ -20,11 +23,11 @@ def calculate_fitness(gen, target, panjang_target):
 
 
 # create population
-def create_population(target, max_population, panjang_target):
+def create_population(target, max_population, population_size):
     populasi = {}
     for i in range(max_population):
-        gen = create_gen(panjang_target)
-        genfitness = calculate_fitness(gen, target, panjang_target)
+        gen = create_gen(population_size)
+        genfitness = calculate_fitness(gen, target, population_size)
         populasi[gen] = genfitness
     return populasi
 
@@ -96,30 +99,35 @@ def display(parent):
 
 
 # main program
-target = 'Hello World!'
-max_population = 10
+target = ''.rjust(40, '1')
+
+max_population = 2
 mutation_rate = 0.2
 
 print('Target Word :', target)
 print('Max Population :', max_population)
 print('Mutation Rate :', mutation_rate)
 
-panjang_target = len(target)
+population_size = 40
 startTime = datetime.datetime.now()
 print('----------------------------------------------')
 print('{}\t{}\t{}'.format('The Best', 'Fitness', 'Time'))
 print('----------------------------------------------')
-populasi = create_population(target, int(max_population), panjang_target)
-parent = selection(populasi)
 
-display(parent)
-while 1:
-    child = crossover(parent, target, panjang_target)
-    mutant = mutation(child, target, float(mutation_rate), panjang_target)
-    if bestfitness(parent) >= bestfitness(mutant):
-        continue
-    populasi = regeneration(mutant, populasi)
+success = False
+while not success:
+    max_population = max_population * 2
+    populasi = create_population(target, int(max_population), population_size)
     parent = selection(populasi)
     display(parent)
-    if bestfitness(mutant) >= 100:
-        break
+    while 1:
+        child = crossover(parent, target, population_size)
+        mutant = mutation(child, target, float(mutation_rate), population_size)
+        if bestfitness(parent) >= bestfitness(mutant):
+            continue
+        populasi = regeneration(mutant, populasi)
+        parent = selection(populasi)
+        display(parent)
+        print("end of term")
+        if bestfitness(mutant) >= 100:
+            break
