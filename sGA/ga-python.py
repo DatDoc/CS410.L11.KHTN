83dfe5ba1d87c0fad74c892747957cdec85a78cc
DatDoc <uncledrew2youngblood@gmail.com>
2020-10-15 02:06:08 +0700

    updating..

--- sGA/ga-python.py
+++ sGA/ga-python.py
@@ -1,11 +1,5 @@
 import numpy as np
-import random
 import argparse
-from tqdm import tqdm
-
-number_of_chromosome = 4
-max_chromosome = 8192
-randomSeed = 18520573
 
 
 # calculate fitness of gen
@@ -13,60 +7,59 @@ def calculate_fitness(offspring, option):
     fitness = 0
     if option == "onemax":
         fitness = np.sum(offspring)
-    else:
-        group = []
-        sum = 0
-        for i in range(0, int(len(offspring))):  # if len(offspring) == 30 then number of element in each group are 6
-            if (i + 1) % (len(offspring) / 5) != 0 and i > 0:
-                sum += offspring[i]
-            else:
-                group.append(sum + offspring[i])
-                sum = 0
-        for i in range(0, int(len(offspring) / 5)):
-            if group[i] < len(offspring) / 5:
-                group[i] = len(offspring) / 5 - 1 - group[i]
-
-        for i in range(0, int(len(offspring) / 5)):
-            fitness += group[i]
+    elif option == "trap":
+        if len(offspring) % 5 != 0:
+            print("ERROR: CHROMOSOME SIZE IS NOT DIVISIBLE BY 5")
+        else:
+            sum_of_each_group = 0
+            for i in range(0, int(len(offspring))):
+                # if len(offspring) == 30 then there are 6 groups
+                if (i + 1) % 5 != 0:
+                    sum_of_each_group += offspring[i]
+                else:
+                    sum_of_each_group += offspring[i]
+                    if sum_of_each_group == 5:
+                        fitness += sum_of_each_group
+                    else:
+                        fitness += 5 - sum_of_each_group - 1
+                    sum_of_each_group = 0  # then set the sum to 0 continue the operation
     return fitness
 
 
 # create population
-def create_population(chromosome_size, population):
-    populasi = np.random.randint(0, 2, size=(chromosome_size, population))
-    return populasi
+def create_population(population_len, chromosome_len):
+    population = np.random.randint(2, size=(population_len, chromosome_len))
+    return population
 
 
 # crossover
-def crossover(populasi, option):
-    tmp_pop = populasi.copy()
+def crossover(population, option):
+    tmp_pop = population.copy()
     np.random.shuffle(tmp_pop)
-    # print(tmp_pop)
     clone = []
     if option == "1X":
-        for i in range(0, len(tmp_pop), 2):
-            swap_point = np.random.randint(0, len(tmp_pop[0]))
-            clone.append(np.concatenate([tmp_pop[i][:swap_point], tmp_pop[i + 1][swap_point:]]))
-            clone.append(np.concatenate([tmp_pop[i + 1][:swap_point], tmp_pop[i][swap_point:]]))
-    else:
+        for pos in range(0, len(tmp_pop), 2):
+            swap_pos = np.random.randint(0, len(tmp_pop[0]))
+            clone.append(np.concatenate([tmp_pop[pos][:swap_pos], tmp_pop[pos + 1][swap_pos:]]))
+            clone.append(np.concatenate([tmp_pop[pos + 1][:swap_pos], tmp_pop[pos][swap_pos:]]))
+    elif option == "UX":
         for i in range(0, len(tmp_pop), 2):
             clone.append(tmp_pop[i])
             clone.append(tmp_pop[i + 1])
             for j in range(0, len(tmp_pop[i])):
                 rand = np.random.rand()
-                # print(j, " ", rand)
                 if rand >= 0.5:
                     clone[i][j], clone[i + 1][j] = tmp_pop[i + 1][j], tmp_pop[i][j]
     return clone
 
 
-def Popop(population, offsprings):
-    return np.concatenate([population, offsprings])
+def Popop(population, babies):
+    return np.concatenate([population, babies])
 
 
-# tourament selection
-def tournament_selection(populasi, option):
-    tmp_pop = populasi.copy()
+# tournament selection
+def tournament_selection(population, option):
+    tmp_pop = population.copy()
     np.random.shuffle(tmp_pop)
     new_gen = []
     for i in range(0, len(tmp_pop), 4):
@@ -86,56 +79,63 @@ def tournament_selection(populasi, option):
     return new_gen
 
 
-def check_converged(populasi):
-    if np.all(populasi == populasi[0]):
+def check_converged(population):  # check whether the population converged to the same string
+    if np.all(population == population[0]):
         return True
     else:
         return False
 
 
-def is_success(populasi, target_string):
-    if np.all(populasi == target_string):
+def is_success(population, target_string):  # check whether the whole population is converged to 1
+    if np.all(population == target_string):
         return True
     else:
         return False
 
 
-def run(chromosome_size, population, type_of_crossover, type_of_fitness):
-    populasi = create_population(chromosome_size, population)  # create population
+def run(population_len, chromosome_len, type_of_crossover, type_of_fitness):
+    population = create_population(population_len, chromosome_len)  # create population
     while True:
-        if check_converged(populasi):
+        if check_converged(population):
             break
-        offsprings = crossover(populasi, type_of_crossover)  # 2 types of crossover: UX and 1X
-        popop = Popop(populasi, offsprings)
-        new_gen1 = tournament_selection(popop, type_of_fitness)
-        new_gen2 = tournament_selection(popop, type_of_fitness)
-        populasi = np.concatenate([new_gen1, new_gen2])
+        offsprings = crossover(population, type_of_crossover)  # 2 types of crossover: UX and 1X
+        popop = Popop(population, offsprings)
+        new_gen = np.concatenate([tournament_selection(popop, type_of_fitness),
+                                  tournament_selection(popop, type_of_fitness)])
+        population = new_gen
         global number_of_calling_fitness
-        number_of_calling_fitness = number_of_calling_fitness + number_of_chromosome * 8
-    return populasi
+        # number_of_calling_fitness : number of time calling fitness function
+        number_of_calling_fitness = number_of_calling_fitness + population_size * 4
+    return population
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument("-population_size", "--number of parameters", required=True, type=int, help="number of parameters")
-    ap.add_argument("-function_type", "--function type", required=True, help="Type of function")
+    ap.add_argument("-chromosome_size", "--number of chromosome", required=True, type=int, help="number of chromosome")
+    ap.add_argument("-fitness_type", "--fitness type", required=True, help="Type of fitness function")
     ap.add_argument("-crossover_type", "--crossover type", required=True, help="Type of crossover")
     args = vars(ap.parse_args())
-    population_size = args["number of parameters"]
-    fitness_func = args["function type"]
+    chromosome_size = args["number of chromosome"]
+    fitness_type = args["fitness type"]
     crossover_type = args["crossover type"]
     f = open('result.txt', 'a')
-    f.write('\n- population_size = {}, fitness function: {}, crossover: {}\n'.format(population_size, fitness_func,
+    f.write('\n- chromosome_size = {}, fitness function: {}, crossover: {}\n'.format(chromosome_size, fitness_type,
                                                                                      crossover_type))
-    target = [1] * population_size
+    max_population = 8192
+    randomSeed = 18520573
+    #  chromosome_size: number of gene
+    #  population_size: number of chromosome
+    #  max_population: maximum number of a population
+    #  randomSeed: generate different population each loop
+    target = [1] * chromosome_size  # define the target to be an array full of 1
     MRPSs = []
     number_of_evaluation = []
-    for bisection in range(0, 100, 10):
+    for bisection in range(0, 100, 10):  # each bisection run 10 randomSeed
         print('{}-th bisection'.format(int(bisection / 10) + 1))
         f.write("\t{}-th bisection\n".format(int(bisection / 10) + 1))
-        global number_of_calling_fitness
         number_of_calling_fitness = 0
         randomSeed += bisection
+        population_size = 4
 
         # STAGE 1
         N_upper = 0
@@ -145,19 +145,19 @@ if __name__ == "__main__":
             pass_all_seed = True
             for seed in range(randomSeed + 0, randomSeed + 10):
                 np.random.seed(seed)
-                result = run(number_of_chromosome, population_size, crossover_type, fitness_func)
+                result = run(population_size, chromosome_size, crossover_type, fitness_type)
                 if not is_success(result, target):
                     pass_all_seed = False
                     break
 
             if pass_all_seed:
-                print(number_of_chromosome)
+                print(population_size)
                 print(number_of_calling_fitness)
-                N_upper = number_of_chromosome
+                N_upper = population_size
                 f.write("\t\t\tN_upper after stage 1: {}\n".format(N_upper))
                 break
-            number_of_chromosome = number_of_chromosome * 2
-            if number_of_chromosome > max_chromosome:
+            population_size = population_size * 2
+            if population_size > max_population:
                 print("ERROR: CROSS THE LIMIT OF NUMBER OF MAXIMUM CHROMOSOME")
                 f.write("\t\t\tN_upper exceed 8192\n")
                 exceed = True
@@ -174,7 +174,7 @@ if __name__ == "__main__":
                 pass_all_seed = True
                 for i in range(randomSeed + 0, randomSeed + 10):
                     np.random.seed(i)
-                    result = run(N, population_size, crossover_type, fitness_func)
+                    result = run(N, chromosome_size, crossover_type, fitness_type)
                     if not is_success(result, target):
                         pass_all_seed = False
                         break
